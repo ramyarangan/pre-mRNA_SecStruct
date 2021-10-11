8ad0170376b6f49b4ab4ea683a8d27fa4077f885
ramyarangan <ramyar@stanford.edu>
2021-10-11 15:49:52 -0700

    Edit plot dG zipper stems

--- src/analyses/plot_dg_zipper_stem.py
+++ src/analyses/plot_dg_zipper_stem.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 
 from util import features_db
-
+from util.features_db import * 
 
 intron_class = sys.argv[1] # E.g. standard_allsize_min_50_max_600
 control_class = sys.argv[2] # E.g. standard_allsize_min_50_max_600_shuffle
@@ -17,6 +17,11 @@ secstruct_options = {'secstruct_pkg': 'Vienna',
 					}
 
 all_features = ["ZipperStemStartMetric", "ZipperStemEndMetric"]
+
+summary_cutoffs = {
+	"ZipperStemStartMetric": -10, 
+	"ZipperStemEndMetric": -10
+}
 
 metric_names = [features_db.get_feature_full_name(feature_name, secstruct_options) \
 		for feature_name in all_features]
@@ -38,6 +43,44 @@ control_feature_df = features_db.get_features(all_features, \
 	control_class, feature_options_all=feature_options_all)
 control_feature_df = control_feature_df.dropna(axis=0)
 
+# Print number of introns 
+check_update_secstruct(intron_class, secstruct_options)
+all_introns = build_intron_set(intron_class, secstruct_options)
+intron_len_mask = [(len(x.seq) > 200) for x in all_introns.introns]
+intron_len_mask = np.array(intron_len_mask)
+print("Total introns with length > 200: %d" % sum(intron_len_mask))
+
+if len(standard_feature_df) != len(all_introns.introns):
+	print("Don't have the expected number of features")
+
+# Get number of stems that pass threshold 
+for ii, feature in enumerate(all_features):
+	total_standard = len(standard_feature_df)
+	total_control = len(control_feature_df)
+
+	summary_cutoff_val = summary_cutoffs[feature]
+	
+	mask = standard_feature_df[metric_names[ii]] < summary_cutoff_val
+	mask_len = np.logical_and(np.array(mask), intron_len_mask)
+	num_passing_standard = sum(mask)
+	num_passing_standard_len = sum(mask_len)
+
+	mask = control_feature_df[metric_names[ii]] < summary_cutoff_val
+	mask_len = np.logical_and(np.array(mask), intron_len_mask)
+	num_passing_control = sum(mask)
+	num_passing_control_len = sum(mask_len)
+
+	print(feature)
+	print("Num passing cutoff %f kcal/mol - introns: %d/%d" % \
+		(summary_cutoff_val, num_passing_standard, total_standard))
+	print("Num passing cutoff %f kcal/mol - controls: %d/%d" % \
+		(summary_cutoff_val, num_passing_control, total_control))
+	print("Num passing cutoff %f kcal/mol with len > 200 - introns: %d/%d" % \
+		(summary_cutoff_val, num_passing_standard_len, sum(intron_len_mask)))
+	print("Num passing cutoff %f kcal/mol with len > 200 - controls: %d/%d" % \
+		(summary_cutoff_val, num_passing_control_len, sum(intron_len_mask)))
+
+# Get histogram bin values - used for plotting in GraphPad Prism
 for ii, feature in enumerate(all_features):
 	mask = standard_feature_df[metric_names[ii]] < -0.05
 	counts, bins, bars = plt.hist(standard_feature_df[mask][metric_names[ii]], \
