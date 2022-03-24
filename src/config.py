7ef8658fd310dd633649fa3e71f70ce05c59db41
ramyarangan <ramyar@stanford.edu>
2022-03-24 16:31:05 -0700

    remove config.py

--- src/config.py
+++
@@ -1,3 +0,0 @@
-DATABASE_PATH='/Users/ramyar/Dropbox/GradSchool/Research/splicing/secstructs/pre-mRNA_SecStruct/database/'
-TMP_PATH='/Users/ramyar/Dropbox/GradSchool/Research/splicing/secstructs/pre-mRNA_SecStruct/src/util/tmp/'
-PATH_VARNA_JAR = '/Users/ramyar/Dropbox/GradSchool/Research/packages/VARNAv3-93.jar'
